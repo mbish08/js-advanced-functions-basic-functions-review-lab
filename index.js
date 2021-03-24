ab302eabacf09db44c37ab69e529f739c8517694
mbish08 <mdrew8@gmail.com>
2021-03-23 17:25:20 -0700

    commit tree 990878c35e23548c3213c17e5a04242ed0c272c5

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(work="go to the office") {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(flair="*") {
+    return function(something="special") {
+        return `You are ${flair}${something}${flair}!`
+    }
+}
+
+const Calculator =  {
+    add: function(a, b) {
+        return a + b
+    },
+    subtract: function(a, b) {
+        return a - b
+    },
+    multiply: function(a, b) {
+        return a * b
+    },
+    divide: function(a, b) {
+        return a / b
+    }
+}
+
+function actionApplyer(start, ray) {
+    let a = start
+
+  for (let i = 0; i < ray.length; i++ ){
+    a = ray[i](a)
+  }
+
+  return a
+}
